e7de21658b7872bb091b0b1be709d4fc35a884ee
Andrew Vargas <vargasandrew2017@gmail.com>
2022-12-14 21:14:25 -0800

    Initial Commit

---
+++ src/server/trpc/router/auth.ts
@@ -0,0 +1,13 @@
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+
+export const authRouter = router({
+  getSession: publicProcedure.query(({ ctx }) => {
+    return ctx.session;
+  }),
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
